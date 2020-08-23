f329fe2a86608745a9f47e931fc4045e679f156f
Heather Beckman <heatherbeckman85@gmail.com>
2020-08-23 08:25:16 -0600

    got programmers to work!

--- src/components/Programmers.js
+++ src/components/Programmers.js
@@ -29,13 +29,14 @@ export default function Programmers() {
   // The programmers list on the one hand, and the id of the featured programmer on the other.
   const [ list, setList ] = useState(listOfAwesome);
   const [featured, setFeatured] = useState(null);
-  
+
   const getNameOfFeatured = () => {
     // Leave this for last!
     // This is NOT an event handler but a helper function. See its usage inside the JSX.
     // It's going to utilize both slices of state to return the _name_ of the featured dev.
     // The beauty of closures is that we can "see" both slices of state from this region
     // of the program, without needing to inject the information through arguments.
+    return list[featured - 1].name;
   };
 
   const style = {
@@ -52,9 +53,9 @@ export default function Programmers() {
           /* Nasty bug! We should map over a slice of state, instead of 'listOfAwesome'.
           We might think: "it works, though!" But if the list of programmers is not state,
           we could never add or edit programmers in the future. The list would be a static thing." */
-          listOfAwesome.map(dev =>
+          list.map(dev =>
             <div key={dev.id}>
-              {dev.name} <button onClick={() => { /* in here set the featured id to be dev.id */ }}>Feature</button>
+              {dev.name} <button onClick={() => { setFeatured(dev.id) /* in here set the featured id to be dev.id */ }}>Feature</button>
             </div>
           )
         }
@@ -63,7 +64,7 @@ export default function Programmers() {
         // Ternaries are fantastic to render "one thing or the other" depending on the "truthiness" of something.
         // Pseudo-code: if the currently featured id is truthy render div 1, otherwise render div 2.
         // Replace the hard-coded false with the correct variable.
-        false
+        featured
           ? <div style={style}>🎉 Let&apos;s celebrate {getNameOfFeatured()}! 🥳</div>
           : <div style={style}>Pick an awesome programmer</div>
       }
