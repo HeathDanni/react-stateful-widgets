6872d0a270009be5a90c549e9c57a10c775c3900
Heather Beckman <heatherbeckman85@gmail.com>
2020-08-23 07:44:04 -0600

    created two slices of state

--- src/components/Programmers.js
+++ src/components/Programmers.js
@@ -27,7 +27,9 @@ const listOfAwesome = [
 export default function Programmers() {
   // We'll have to use the state hook twice, as we need two slices of state.
   // The programmers list on the one hand, and the id of the featured programmer on the other.
-
+  const [ list, setList ] = useState(listOfAwesome);
+  const [featured, setFeatured] = useState(null);
+  
   const getNameOfFeatured = () => {
     // Leave this for last!
     // This is NOT an event handler but a helper function. See its usage inside the JSX.
